03a05d89560828573d4f48477e4f76e29c29f4dc
moonhyeonah <moonhyeonah@gmail.com>
2020-02-23 05:22:41 -0400

    update 2020-02-03

--- test09.py
+++ test09.py
@@ -15,46 +15,24 @@ with open('test08.sol') as f:
 # Create one user account
 # And deploy the contract
 user_account = m.create_account(balance=1000)
+#from_account = m.create_account(balance=1000)
 m.create_account(balance=1000)
 m.create_account(balance=1000)
 from_account = m.make_symbolic_value()
 to_account = m.make_symbolic_value()
 m.constrain(from_account != to_account)
-#print("from_account : 0x%x (%d)"%(from_account, from_account))
-#print("to_account : 0x%x (%d)"%(to_account, to_account))
 
 contract_account = m.solidity_create_contract(source_code, owner=user_account, balance=0)
-contract_account.balanceOf(to_account, caller=user_account)
-contract_account.balanceOf(from_account, caller=user_account)
+contract_account.balanceOf(to_account)
+contract_account.balanceOf(from_account)
 
 symbolic_val1 = m.make_symbolic_value()
 
 contract_account.transfer(to_account, symbolic_val1, caller=from_account)
-contract_account.balanceOf(from_account, caller=user_account)
-contract_account.balanceOf(to_account, caller=user_account)
+contract_account.balanceOf(from_account)
+contract_account.balanceOf(to_account)
 
-for state in m.ready_states:    
-    
-    input = state.input_symbols[0]
-    input = Operators.CONCAT(256, *input[4:36])
-    print("input0 : {}".format(input))
-
-    input = state.input_symbols[1]
-    input = Operators.CONCAT(256, *input[4:36])
-    print("input1 : {}".format(input))
-
-    input = state.input_symbols[2]
-    input = Operators.CONCAT(256, *input[4:36])
-    print("input2 : {}".format(input))
-
-    input = state.input_symbols[3]
-    input = Operators.CONCAT(256, *input[4:36])
-    print("input3 : {}".format(input))
-    
-    input = state.input_symbols[4]
-    input = Operators.CONCAT(256, *input[4:36])
-    print("input4 : {}".format(input))
-    
+for state in m.ready_states:
     balance_before = state.platform.transactions[1].return_data
     balance_before = ABI.deserialize("uint", balance_before)
 
@@ -66,3 +44,4 @@ for state in m.ready_states:
     if solver.check(state.constraints):
         print("Overflow found! see {}".format(m.workspace))
         m.generate_testcase(state, "OverflowFound")
+
